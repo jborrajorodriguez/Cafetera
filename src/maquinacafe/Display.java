f32928915aee9f69dddb876721bb16c68e3aee2f
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-16 14:47:09 +0100

    Clase display con mas metodos

--- src/maquinacafe/Display.java
+++ src/maquinacafe/Display.java
@@ -6,16 +6,26 @@ import javax.swing.JOptionPane;
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class Display {
+    //private static float credito;
     
-    public float dineroIN(){
-        float dinero=Float.parseFloat(JOptionPane.showInputDialog("Introduce el dinero"));
-        return dinero;
+    public static float dineroIN(){
+        Monedero.setCredito(Float.parseFloat(JOptionPane.showInputDialog("Introduce el dinero")));
+        return Monedero.getCredito();
     }
     
     public void productoAcabado(boolean a){
         if(a==true){
             JOptionPane.showMessageDialog(null,"Retire Producto");
         }else{System.out.println("Esperando");}
+    }
+    
+    public static void visualizarVueltaCompletado(float a){
+        JOptionPane.showMessageDialog(null,"Aqui tiene su vuelta "+a+" €");
+    }
+    
+    public static float mostrarCredito(){
+        JOptionPane.showMessageDialog(null,"Actualmente hay "+Monedero.getCredito());
+        return Monedero.getCredito();
     }
 
 }
