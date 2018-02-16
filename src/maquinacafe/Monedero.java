1846290f3207d68d9873186d67d393a1fd94050f
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-16 14:47:39 +0100

    Clase monedero con mas metodos

--- src/maquinacafe/Monedero.java
+++ src/maquinacafe/Monedero.java
@@ -1,5 +1,6 @@
 package maquinacafe;
 
+import javax.swing.JOptionPane;
 import maquinacafe.Display;
 
 /**
@@ -7,60 +8,72 @@ import maquinacafe.Display;
  */
 public class Monedero {
 
-    private float credito, precio, cambio;
+    private static float credito, precio=1, cambio;
 
     public Monedero() {
     }
 
-    public Monedero(float credito,float precio,float cambio) {
-        this.credito=credito;
-        this.precio=precio;
-        this.cambio=cambio;
-    }
-
-    public float getCredito() {
+    public static float getCredito() {
         return credito;
     }
 
-    public void setCredito(float credito) {
-        this.credito=credito;
+    public static void setCredito(float credito) {
+        Monedero.credito=credito;
     }
 
-    public float getPrecio() {
+    public static float getPrecio() {
         return precio;
     }
 
-    public void setPrecio(float precio) {
-        this.precio=precio;
+    public static void setPrecio(float precio) {
+        Monedero.precio=precio;
     }
 
-    public float getCambio() {
+    public static float getCambio() {
         return cambio;
     }
 
-    public void setCambio(float cambio) {
-        this.cambio=cambio;
+    public static void setCambio(float cambio) {
+        Monedero.cambio=cambio;
     }
 
-    public boolean inDinero(float dinero) {
+    public static boolean inDinero(float dinero) {
         boolean b;
-        if (dinero==1) {
+        if (dinero==precio) {
             System.out.println("Dinero exacto");
             return b=true;
         }
-        else if (dinero>1) {
+        else if (dinero>precio) {
             System.out.println("Elige producto y espere el cambio");
             return b=true;
 
         }
         else {
-            cambio=1-dinero;
+            cambio=precio-dinero;
             System.out.println("Dinero insuficiente\n Tiene que añadir : "+cambio);
             return b=false;
         }
-        
 
     }
-    
+
+    public static float devolverDineroDeMas() {
+        float total=0;
+        if (credito>precio) {
+            total=credito-precio;
+        }
+        return total;
+
+    }
+    public static float añadirMasDinero(){
+        float mas=0;
+        if(credito<precio){
+            while(credito<precio){
+            Display.mostrarCredito();
+            mas=Float.parseFloat(JOptionPane.showInputDialog("Añada mas dinero"));
+            credito=credito+mas;
+            Display.mostrarCredito();
+                    }
+        }return credito;
+    }
 
 }
