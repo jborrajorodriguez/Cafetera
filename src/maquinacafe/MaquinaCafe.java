77dcafb877981f7550e3cb5d46bafb6aab95fcd0
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-16 09:35:47 +0100

    Clase maquina de cafe (main)

---
+++ src/maquinacafe/MaquinaCafe.java
@@ -0,0 +1,31 @@
+package maquinacafe;
+
+import java.util.ArrayList;
+
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class MaquinaCafe {
+
+    public static void main(String[] args) {
+        Botonera boton=new Botonera();
+        Display dis=new Display();
+        Monedero mon=new Monedero();
+        Producto pro=new Producto();
+        ArrayList <Producto> nombreProducto=new ArrayList();
+        nombreProducto.add(new Producto(1,1,"Café"));
+        nombreProducto.add(new Producto(1,1,"Te"));
+        nombreProducto.add(new Producto(1,1,"Chocolate"));
+       
+        
+        dis.productoAcabado(pro.elaborar(boton.elegirProducto(nombreProducto,mon.inDinero(dis.dineroIN()))));
+        
+//        float f=dis.dineroIN();
+//        boolean aa=mon.inDinero(f);
+//        boolean bb=boton.elegirProducto(nombreProducto,aa);
+//        boolean cc=pro.elaborar(bb);
+//        dis.productoAcabado(cc);
+        
+    }
+
+}
